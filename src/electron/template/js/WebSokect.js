4a7cd95a934de304ad7e947d5a0e01b8ad194477
Frank-Paredes <frankparedesalpi@gmail.com>
2022-03-12 17:57:55 -0500

    second commit

--- src/electron/template/js/WebSokect.js
+++ src/electron/template/js/WebSokect.js
@@ -1,10 +1,20 @@
 const WEBSOCKET_ADDRESS = "ws://127.0.0.1:8003";
 
 let socket;
+let message;
+
 function ConectWithServer(){
    socket = new WebSocket("ws://127.0.0.1:8003");
 
    socket.addEventListener("open",()=>{
        console.log("The conection Established with websocket server");
    })
+}
+
+function send(message){
+    socket.send(message);
+}
+
+function response(){
+    
 }
